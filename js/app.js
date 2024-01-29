e0e9c14d7b03f61c1e2b8465161cc43b107482c3
Vitor Araujo Ramos <vitor.ramos.ara@gmail.com>
2024-01-29 17:08:53 -0300

    amigo-secreto-sorteio

---
+++ js/app.js
@@ -0,0 +1,57 @@
+let amigos = []; // criando array
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == ''){
+        alert('Informe o nome do amigo');
+        return; //encerra a função pra nao incluir nada
+    }
+    
+    if(amigos.includes(amigo.value)){
+        alert('Nome ja adicionado, inserir outro nome');
+        return;
+    }
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value); //adiciona info dentro do array
+    if(lista.textContent == ''){
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+}
+
+function sortear() { 
+    if(amigos.length < 4){
+        alert('Adicione pelo menos 4 amigos');
+        return;
+    }
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for(let i = 0; i < amigos.length; i++){
+        if (i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>';
+        } else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>';
+        }
+    }
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML  = '';
+}
